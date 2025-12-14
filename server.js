011d2403f63bd4ee5a748c68c956045b6148659d
IvanPasciucco <54645974+IvanPasciucco@users.noreply.github.com>
2025-12-14 17:28:04 -0300

    first commit5

--- server.js
+++ server.js
@@ -2,61 +2,31 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const bcrypt      = require('bcrypt');
 const app         = express();
+const bcrypt      = require('bcrypt');s
+
 fccTesting(app);
+
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
 
-
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res);
+  });
+});
 
 //END_ASYNC
 
 //START_SYNC
-bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
-  console.log(hash); // Muestra el hash generado en consola
-  
-  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
-    console.log(res); // Debería mostrar 'true'
-  });
-});
-
 
 //END_SYNC
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log("Listening on port:", PORT)
+  console.log("Listening on port:", PORT)
 });
